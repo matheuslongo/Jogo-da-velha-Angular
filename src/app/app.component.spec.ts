a31e7da436a72944e79d3d8f24d137c8a449e912
matheuslongo <longo.matheusmacedo@gmail.com>
2022-08-02 11:46:29 -0300

    add

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [JogoDaVelhaModule],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
